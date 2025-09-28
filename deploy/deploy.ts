4f322e4334311946c4ab027f4ab2594af7dea466
AlgernonPunk <klqebqgad713@hotmail.com>
2025-09-28 14:56:35 +0800

    update deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,15 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedSecretBank = await deploy("SecretBank", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`SecretBank contract deployed at: `, deployedSecretBank.address);
+  console.log(`Network: `, hre.network.name);
+  console.log(`Deployer: `, deployer);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_secretBank"; // id required to prevent reexecution
+func.tags = ["SecretBank"];
